651efc2703c240e77d465a7539be367693db876e
Djam18 <adamdjamal18@gmail.com>
2017-09-04 10:30:00 +0100

    Ajout de layouts Blade de base et vue todos/index.blade.php

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Liste de tâches')
+
+@section('content')
+    <h1>Mes Tâches</h1>
+    <ul>
+        <li>Faire les courses</li>
+        <li>Réviser Laravel</li>
+        <li>Boire de l’eau</li>
+    </ul>
+@endsection
