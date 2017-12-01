26beca617cd3ee0de17d9920dd67519ecfb7daa4
Djam18 <adamdjamal18@gmail.com>
2017-12-01 10:00:00 +0100

    hasOne (Un utilisateur a un profil)

--- app/User.php
+++ app/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function profile()
+{
+    return $this->hasOne(Profile::class);
+}
+
 }
