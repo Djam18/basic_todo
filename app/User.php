c8eaafbb6252fc417faf716378e72619bb31b23b
Djam18 <adamdjamal18@gmail.com>
2017-12-05 15:30:00 +0100

    hasMany (Un utilisateur a plusieurs posts)

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function posts(): HasMany
+    public function posts()
     {
         return $this->hasMany(Post::class);
     }
